ea908e3340272e30dfed8291132eefdc03f8e942
jonbirdsall <cuedain@gmail.com>
2016-03-12 01:05:41 +0000

    CreateMicroposts migration was creating fks incorrectly

--- db/migrate/20160307004803_create_microposts.rb
+++ db/migrate/20160307004803_create_microposts.rb
@@ -2,7 +2,7 @@ class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
       t.text :content
-      t.references :user, index: true, foreign_key: true
+      t.references :user, index: true
 
       t.timestamps null: false
     end
